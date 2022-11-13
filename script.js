b7e77e8d89260db5f2f7ac136e7e912186e128a7
justniktoq <116247290+justniktoq@users.noreply.github.com>
2022-11-13 17:33:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+/*
+let age = confirm('Вас зовут Миша?')
+function checkAge () {
+  if (age === true) {
+    let age = confirm('Ваша фамилия Коршак?')
+    if (age === true ) {
+      alert ('Проходите.');
+    }
+    else {
+      alert('Идите нахуй!');
+    }
+  }
+  else {
+    alert('Идите нахуй!')
+  }
+}
+checkAge();*/
